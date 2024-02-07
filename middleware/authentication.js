dc4ec5f2726e03e5bfb80dadb790c7d1fc188507
Pranav Patil <130309887+pra9patil@users.noreply.github.com>
2024-02-07 12:39:36 +0530

    Add files via upload

---
+++ middleware/authentication.js
@@ -0,0 +1,23 @@
+const { validateToken } = require("../services/authentication");
+
+function checkForAuthenticaionCookie(cookieName){
+
+    return (req , res,next) => {
+        const tokenCookieValue = req.cookies[cookieName];
+
+        if(!tokenCookieValue) {
+           return  next();
+        }
+
+        try {
+            const userPayload = validateToken(tokenCookieValue);
+            req.user = userPayload
+        } catch (error){ }
+
+       return  next()
+    };
+}
+
+module.exports = {
+    checkForAuthenticaionCookie,
+}
